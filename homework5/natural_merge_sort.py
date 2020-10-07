b35825f0bb02a1230b717e3cb0e363933aee1868
WhiteHyun <cx_nf@naver.com>
2020-10-07 15:55:02 +0900

    HW5: Natural merge sort 속도 개선

--- homework5/natural_merge_sort.py
+++ homework5/natural_merge_sort.py
@@ -19,21 +19,32 @@ def natural_merge_sort(arr: list, n: int):
     run = find_run(arr, n)  # 런을 구함
     while len(run) != 1:    # 런의 크기가 1이 될 때까지 반복
         # print(run)
-        for i in range(len(run)//2):  # 런 합병 사이클 반복문
-            part1 = run.pop(i)  # 두 런 중에 앞의 런만 pop하여 뒤의 런에 값 할당
-            part2 = run[i]
+        new_run = []  # run 변수에 덮어 씌울 새로운 런 리스트 생성
+        for i in range(0, len(run), 2):  # 런 합병 사이클 반복문
+            temp_run = []   # 두 개의 런을 합병하여 new_run에 append할 임시 리스트
+            part1 = run[i]
+            if len(run)-1 >= i+1:
+                part2 = run[i+1]
+            else:
+                part2 = []
             j = 0
             k = 0
             while len(part1) != j and len(part2) != k:
                 if part1[j] < part2[k]:
-                    part2.insert(k, part1[j])
+                    temp_run.append(part1[j])
                     j += 1
-                    k += 1
                 else:
+                    temp_run.append(part2[k])
                     k += 1
             while len(part1) != j:
-                part2.append(part1[j])
+                temp_run.append(part1[j])
                 j += 1
+            while len(part2) != k:
+                temp_run.append(part2[k])
+                k += 1
+            new_run.append(temp_run)
+        run = new_run
+
     # 기존 array에 정렬 값 저장
     for i in range(1, n+1):
         arr[i] = run[0][i-1]
@@ -54,7 +65,7 @@ def check_sort(arr, n):
 
 
 if __name__ == "__main__":
-    N = 10000
+    N = 100000
     # key = [-1, 6, 7, 8, 3, 4, 1, 5, 9, 10, 2]
     key = list(range(1, N+1))
     random.shuffle(key)

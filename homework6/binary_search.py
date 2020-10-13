7428e205c0697669e4b64e6882002313b4a1ed59
WhiteHyun <cx_nf@naver.com>
2020-10-14 02:21:48 +0900

    HW6: Fix typo, delete main codes

--- homework6/binary_search.py
+++ homework6/binary_search.py
@@ -1,8 +1,5 @@
 from init import *
 
-import random
-import time
-
 
 class BinarySearch(Tree):
 
@@ -24,29 +21,9 @@ class BinarySearch(Tree):
         else:
             p.right = x
 
-    def check(self, search_list: list) -> None:
+    def check(self, key_list: list) -> None:
         """
         Tree의 추상메서드입니다.
         이진탐색트리에서는 check를 사용하지 않습니다.
         """
         return True
-
-
-if __name__ == "__main__":
-    N = 20000
-    # key = [2, 1, 7, 8, 6, 3, 5, 4]
-    key = list(range(1, N+1))
-    s_key = list(range(1, N+1))
-    random.shuffle(key)
-    # key.sort(reverse=True)
-    d = BinarySearch()
-    for i in key:
-        d.insert(i)
-    start_time = time.time()
-    for i in s_key:
-        result = d.search(i)
-        if result == -1 or result != i:
-            print('탐색 오류')
-    end_time = time.time()-start_time
-    print(f'이진 트리 탐색(랜덤)의 실행 시간 (N= {N}) : {end_time:.3f}')
-    print('탐색 완료')

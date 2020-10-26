fa1c9615c5888854ac9bd28aa5dbefcd0c8e17a0
whitehyun <cx_nf@naver.com>
2020-10-26 20:08:06 +0900

    HW7: Make Tree class abstract, add func from node

--- homework7/init.py
+++ homework7/init.py
@@ -1,3 +1,6 @@
+from abc import ABCMeta, abstractmethod
+
+
 class Node:
     """
     탐색트리에 사용되는 노드 클래스입니다.
@@ -9,7 +12,7 @@ class Node:
         bits (str): key값에 따른 이진수를 문자열입니다.
     """
 
-    def __init__(self, key, left=None, right=None):
+    def __init__(self, key: int, left=None, right=None):
         """
         노드를 초기화 해주는 함수입니다.
 
@@ -23,11 +26,11 @@ class Node:
         Returns:
             None
         """
-        temp_num = bin(key)[2:]
+
         self.__key = key
         self.__left = left
         self.__right = right
-        self.__bits = "0" * (5 - len(temp_num)) + temp_num  # 5자리의 2진수로 수정
+        self.__bits = self.key_to_bin(key)
 
     @property
     def key(self):
@@ -61,6 +64,13 @@ class Node:
     def bits(self, bits):
         self.__bits = bits
 
+    def key_to_bin(self, key: int) -> str:
+        """
+        key값을 5자리의 2진수 형태로 나타내어 보여줍니다.    
+        """
+        temp_num = bin(key)[2:]
+        return "0" * (5 - len(temp_num)) + temp_num  # 5자리의 2진수로 수정
+
 
 class Tree():
     """
@@ -72,3 +82,41 @@ class Tree():
 
     def __init__(self) -> None:
         self.head: Node = Node(0)
+
+    @abstractmethod
+    def insert(self, key: int) -> int:
+        """
+        각 트리에 따른 노드를 넣는 함수입니다.
+
+        Args:
+
+            key (int): 노드의 key값을 넣기 위한 파라미터입니다.
+
+        Returns:
+            0: Success
+            -2: Error
+        """
+        pass
+
+    @abstractmethod
+    def search(self, search_key: int) -> int:
+        """
+        각 트리에 따른 탐색 함수입니다.
+
+        Args:
+
+            search_key (int): 트리 내에서 탐색할 key 값입니다.
+
+        Returns:
+            0: Success
+            -1: Search Failed
+            -2: Search Error
+        """
+        pass
+
+    @abstractmethod
+    def check(self, key_list: list, time: float) -> None:
+        """
+        트리가 정확하게 구축되었는지 확인하여 출력해주는 메서드입니다.
+        """
+        pass

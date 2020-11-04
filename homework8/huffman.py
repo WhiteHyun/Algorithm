2e1d39b439791e19293c737cb5af06702d56f99d
whitehyun <cx_nf@naver.com>
2020-11-04 22:09:13 +0900

    HW8: Fix Typo, Add comments

--- homework8/huffman.py
+++ homework8/huffman.py
@@ -92,7 +92,9 @@ class Huffman():
     Attributes:
         text (str): 문자열 값입니다. 이 값을 가지고 허프만 트리 알고리즘이 만들어집니다.
         count (list): text에서 각 문자당 빈도수 나타내는 리스트입니다.
-        parent (list): 허프만 트라이가 구현되었을 때 인덱스마다 각 노드의 부모인덱스를 가리킵니다. 자신이 부모노드의 왼쪽자식이면 양수, 오른쪽자식이면 음수로 나타냅니다.
+        parent (list): 허프만 트라이가 구현되었을 때 인덱스마다 각 노드의 부모인덱스를 가리킵니다. 
+                       자신이 부모노드의 왼쪽자식이면 양수, 오른쪽자식이면 음수로 나타냅니다.
+                       속칭 dad라고도 불리웁니다.
         heap (Heap): 힙 트리를 구성합니다. 힙에 대해서는 Heap.__doc__() 명령어로 알아보실 수 있습니다.
         code (list): encoding에
         length (list):
@@ -159,7 +161,7 @@ class Huffman():
         허프만 트리를 구축합니다. 기수 탐색 트라이의 개념으로 접근하여 만들어집니다.
 
         Returns:
-            i-1 (int): 트라이의 가장 끝 인덱스를 나타냅니다. decode에서 사용되어집니다.
+            i-1 (int): 트라이의 최상단노드의 인덱스를 나타냅니다. decode에서 사용되어집니다.
         """
         i = CHAR_LENGTH
         while not self.heap.is_empty():
@@ -181,19 +183,21 @@ class Huffman():
         구현된 트라이를 가지고 코드와 길이를 구합니다.
         """
         for i in range(CHAR_LENGTH):
-            code = 0
-            length = 0
-            digit = 1
+            code = 0    # code[k]를 구하기 위해 사용되는 변수
+            length = 0  # length[k]를 구하기 위해 사용되는 변수
+            digit = 1  # 자리수를 나타내는 변수이며 코드의 값을 구할 때 사용되어집니다.
 
             if self.__count[i]:
-                temp = self.__parent[i]
-                while temp != 0:
-                    if temp < 0:
-                        temp = -temp
+                # parent_index, 자신의 parent값을 인덱스로 사용합니다.
+                p_index = self.__parent[i]
+                while p_index != 0:  # 최상단 인덱스를 참조할 때 까지
+                    if p_index < 0:  # dad값이 음수일 때
+                        p_index = -p_index
+                        # 1일 때를 음수 값으로 설정했으므로 해당 자리수를 1로 설정
                         code += digit
-                    temp = self.__parent[temp]
-                    digit *= 2
-                    length += 1
+                    p_index = self.__parent[p_index]  # 다시 parent 참조
+                    digit <<= 1  # 자리수 왼쪽 시프트
+                    length += 1  # 길이 1 증가
             self.__code[i] = code
             self.__length[i] = length
 

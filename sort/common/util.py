0b5cf5b27a27cd00d0ed61c45bee5b4498b03c1c
whitehyun <cx_nf@naver.com>
2020-12-02 13:35:17 +0900

    Add util.py for sort algorithm

---
+++ sort/common/util.py
@@ -0,0 +1,17 @@
+import time
+import copy
+import random
+
+
+def average(collection: list) -> float:
+    """리스트의 평균값을 구한다.
+
+    Example:
+        >>> average([3, 4, 5])
+        4.0
+    """
+    return sum(collection) / len(collection)
+
+
+def swap(list, a, b):
+    list[a], list[b] = list[b], list[a]

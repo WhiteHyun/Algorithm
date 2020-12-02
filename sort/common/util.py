a3b799ab9d9c17ea6640ac3bcea410e6371d6351
whitehyun <cx_nf@naver.com>
2020-12-02 23:55:55 +0900

    Add digit function

--- sort/common/util.py
+++ sort/common/util.py
@@ -19,3 +19,31 @@ def copy_list(origin):
 
 def swap(list, a, b):
     list[a], list[b] = list[b], list[a]
+
+
+def get_digit(number, position=-1):
+    """숫자의 자리수를, 또는 해당 위치의 값을 구한다.
+
+    Example:
+        >>> get_digit(15427)
+        5
+
+        >>> get_digit(15427, 2)
+        4
+
+        >>> get_digit(49)
+        2
+
+        >>> get_digit(49, 1)
+        4
+    """
+    digit = 0
+    if position == -1:
+        while number != 0:
+            number //= 10
+            digit += 1
+    else:
+        digit = number % (10**(position+1))
+        digit //= 10**(position)
+
+    return digit
